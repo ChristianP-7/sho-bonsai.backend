c082260fa2261b604e3e0acd3b89d138d97acfb7
ChristianP-7 <christianperezt7@gmail.com>
2024-10-15 17:07:04 -0500

    Fix: serverless configuration

--- api/index.js
+++ api/index.js
@@ -1,36 +1,22 @@
-const express = require("express");
-const cors = require("cors");
 const axios = require("axios");
 
-const app = express();
-const port = process.env.PORT || 3000; // Establece un puerto por defecto si no se encuentra el valor en las variables de entorno
-const authorization = process.env.AUT;
-
-// Habilitar CORS para todas las solicitudes
-app.use(cors());
-
-// Ruta para hacer peticiones a la API de eBay
-app.get("/ebay-data", async (req, res) => {
+module.exports = async (req, res) => {
   try {
+    const authorization = process.env.AUT; // Usar la variable de entorno AUT
+
     const response = await axios.get(
       "https://api.ebay.com/buy/browse/v1/item_summary/search?category_ids=181003",
       {
         headers: {
-          Authorization: `Bearer ` + authorization,
+          Authorization: `Bearer ${authorization}`,
         },
       }
     );
-    res.json(response.data);
+    res.status(200).json(response.data);
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        error: "Error al obtener datos de eBay",
-        details: error.message,
-      });
+    res.status(500).json({
+      error: "Error al obtener datos de eBay.",
+      details: error.message,
+    });
   }
-});
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+};
