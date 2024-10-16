f05514b7838c08612564aed4a4d99b443db5d42d
ChristianP-7 <christianperezt7@gmail.com>
2024-10-16 00:49:59 -0500

    Fix: cors

---
+++ api/auth.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+
+let accessToken = null;
+let tokenExpiration = null;
+
+async function getAccessToken() {
+  if (!accessToken || Date.now() > tokenExpiration) {
+    // Si no tenemos un token o si el token ha expirado, lo renovamos
+    const tokenResponse = await axios.post(
+      "https://api.ebay.com/identity/v1/oauth2/token",
+      new URLSearchParams({
+        grant_type: "client_credentials",
+        scope: "https://api.ebay.com/oauth/api_scope",
+      }),
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${process.env.EBAY_CLIENT_ID}:${process.env.EBAY_CLIENT_SECRET}`
+          ).toString("base64")}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    accessToken = tokenResponse.data.access_token;
+    const expiresIn = tokenResponse.data.expires_in; // Tiempo de expiración devuelto por eBay
+    tokenExpiration = Date.now() + expiresIn * 1000; // Ajusta el tiempo de expiración en milisegundos
+  }
+
+  return accessToken;
+}
+
+module.exports = { getAccessToken };
---
+++ api/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors"); // Importar el paquete cors
+const { getAccessToken } = require("./auth");
+const axios = require("axios");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Habilitar CORS para todas las solicitudes
+app.use(
+  cors({
+    origin: "https://sho-bonsai.vercel.app/",
+    methods: ["GET", "POST"], // Opcional: Limita los métodos HTTP permitidos
+    credentials: true, // Si necesitas que se envíen cookies o autenticación en las solicitudes
+  })
+);
+
+app.get("/api/products/:categoryId", async (req, res) => {
+  try {
+    const { categoryId } = req.params;
+    const token = await getAccessToken();
+
+    const ebayResponse = await axios.get(
+      `https://api.ebay.com/buy/browse/v1/item_summary/search?category_ids=${categoryId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.json(ebayResponse.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error al obtener los datos de eBay");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
